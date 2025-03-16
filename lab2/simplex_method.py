32d0a3d932b0db47fba5e934a524e6a7603bef44
Artyom Mazurenko <artemmazurenko71@gmail.com>
2025-03-16 13:07:57 +0300

    fix simplex method

--- lab2/simplex_method.py
+++ lab2/simplex_method.py
@@ -7,18 +7,6 @@ def f(x, coeffs):
     x1, x2 = x[0], x[1]
     return coeffs[0] * x1 ** 2 + coeffs[1] * x2 ** 2 + coeffs[2] * x1 * x2 + coeffs[3] * x1 + coeffs[4] * x2
 
-def constraints(coeffs_con):
-    cons = []
-    for i in range(0, len(coeffs_con), 3):
-        a, b, c = coeffs_con[i], coeffs_con[i+1], coeffs_con[i+2]
-        cons.append({'type': 'ineq', 
-                    'fun': lambda x, a=a, b=b, c=c: c - (a * x[0] + b * x[1])})
-    
-    # Ограничения неотрицательности
-    cons.append({'type': 'ineq', 'fun': lambda x: x[0]})
-    cons.append({'type': 'ineq', 'fun': lambda x: x[1]})
-    return cons
-
 def create_constraints(coeffs_con):
     constraints_list = []
     for i in range(0, len(coeffs_con), 3):
@@ -44,12 +32,12 @@ def optimize(x0, coeffs_f, coeffs_con, opt_type):
     
     if opt_type == "minimize":
         result = minimize(lambda x, coeffs: f(x, coeffs), x0,
-                         args=coeffs_f, constraints=constraints(coeffs_con),
+                         args=coeffs_f, constraints=create_constraints(coeffs_con),
                          options={'maxiter': 1000, 'ftol': 1e-8})
         f_value = result.fun
     elif opt_type == "maximize":
         result = minimize(lambda x, coeffs: -f(x, coeffs), x0,
-                         args=coeffs_f, constraints=constraints(coeffs_con),
+                         args=coeffs_f, constraints=create_constraints(coeffs_con),
                          options={'maxiter': 1000, 'ftol': 1e-8})
         f_value = -result.fun
     
@@ -84,6 +72,7 @@ x0 = [1, 1]
 coeffs_f = [0,-1,0,1,2]  
 coeffs_con = [3,2,6,1,2,4]
 result, success, message = optimize(x0, coeffs_f, coeffs_con, "maximize")
+
 
 """
 x0 = [1, 1]  
